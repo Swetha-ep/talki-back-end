aaa7c82cc42b2c012446978b84a3a5cfce2249b5
swethaep99 <85289802+swethaep99@users.noreply.github.com>
2023-11-09 18:00:40 +0530

    updating the setting.py

--- chat/models.py
+++ chat/models.py
@@ -6,7 +6,7 @@ from Accounts.models import *
 
 # Create your models here.
 class Message(models.Model):
-    sender = models.CharField(null=True)
+    sender = models.CharField(null=True,max_length=200)
     message = models.TextField(null=True)
     thread_name = models.CharField(null=True, max_length=200)
     timestamp = models.DateTimeField(auto_now_add=True)
