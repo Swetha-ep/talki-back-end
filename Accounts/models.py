b75816535d4488c35d55ab75d13bb54abc7cd06a
swethaep99 <85289802+swethaep99@users.noreply.github.com>
2023-11-09 20:42:19 +0530

    database configruation updated

--- Accounts/models.py
+++ Accounts/models.py
@@ -65,8 +65,8 @@ class User(AbstractBaseUser,PermissionsMixin):
 
 class TutorApplication(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    country = models.CharField(max_length=200, blank=True, null=True,default='India')
     name = models.CharField(max_length=255,blank=True, null=True)
-    country = models.CharField(default='India',max_length=200, blank=True)
     phone = models.CharField(max_length=200)  
     about_me = models.TextField()
     teaching_style = models.TextField()
