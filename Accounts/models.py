76efb474ac6f99bb9926bd1b90070d11d9e5eb8f
swethaep99 <85289802+swethaep99@users.noreply.github.com>
2023-11-09 20:47:11 +0530

    model county field changed

--- Accounts/models.py
+++ Accounts/models.py
@@ -65,7 +65,8 @@ class User(AbstractBaseUser,PermissionsMixin):
 
 class TutorApplication(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    country = models.CharField(max_length=200, blank=True, null=True,default='India')
+    # country = models.CharField(max_length=200, blank=True, null=True,default='India')
+    country = models.CharField(max_length=255, blank=True, null=True)
     name = models.CharField(max_length=255,blank=True, null=True)
     phone = models.CharField(max_length=200)  
     about_me = models.TextField()
